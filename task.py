f1821dbaf8073ac88feb90dbf9bd2394536c8b59
Hossein <hossein16akbari@gmail.com>
2025-07-30 14:51:39 +0330

    #9-API for put method

--- task.py
+++ task.py
@@ -10,3 +10,9 @@ def get():
 @task_router.post("/{task}")
 def add(task: str):
     task_list.append(task)
+    return {"tasks": task_list}
+    
+@task_router.put("/")
+def update(index: int, task: str):
+    task_list[index] = task
+    return {"tasks": task_list}
