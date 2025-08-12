8f3abea21328546ecc4ac65ccb3e70304915f97a
Hossein <hossein16akbari@gmail.com>
2025-08-12 12:13:42 +0330

    #20-debugging using Path class

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, Query
+from fastapi import FastAPI, APIRouter, Query, Path
 from task import task_router
 
 app = FastAPI()
@@ -12,9 +12,9 @@ def hello_world() -> dict:
 def add(id: int) -> dict:
     return {"post_id": id}
 
-@app.get("/page")
+@app.get("/page/{page}")
 def page(
-    page: int = Query(1, gt=0, title='page visualizer'),
+    page: int = Path(gt=0, title='page visualizer'),
     size: int = Query(5, ge=5, le=20)
 ):
     return {"page": page, "size": size}
@@ -23,7 +23,7 @@ def page(
 # The phone number is like +34 111 12-34-56
 def phone(
     phone: str = 
-    Query(pattern=r"^(\(?\+[\d]{1,3}\)?)\s?([\d]{1,5})\s?([\d][\s\.-]?){6,7}$" )
+    Path(pattern=r"^(\(?\+[\d]{1,3}\)?)\s?([\d]{1,5})\s?([\d][\s\.-]?){6,7}$" )
 ):
     return {"phone": phone}
 
