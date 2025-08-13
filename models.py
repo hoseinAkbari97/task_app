487265a464f844f306bcd3ce9a99d5373dd94767
Hossein <hossein16akbari@gmail.com>
2025-08-13 15:04:25 +0330

    #27-Creating the Task class for Validation

--- models.py
+++ models.py
@@ -5,3 +5,9 @@ class StatusType(str, Enum):
     """Enum for task status."""
     DONE = "done"
     PENDING = "pending"
+    
+class Task(BaseModel):
+    id: int
+    name: str
+    description: str
+    status: StatusType
