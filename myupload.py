6f3516f136f1668eeb18260c192efaedeaa8327b
hoseinAkbari97 <hossein16akbari@gmail.com>
2026-01-14 14:49:03 +0330

    #57- saving uploaded file

--- myupload.py
+++ myupload.py
@@ -13,3 +13,10 @@ def upload_file1(file: UploadFile):
         "filename": file.filename, 
         "content_type": file.content_type,
         }
+    
+@upload_router.post("/uploadfile2/")
+def upload_file2(file: UploadFile):
+    
+    with open(f"img/uploaded_{file.filename}", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"filename": file.filename}
