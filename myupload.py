32356a97962496bfbd9fececdf4886f87cdbf4ad
hoseinAkbari97 <hossein16akbari@gmail.com>
2026-01-14 14:34:58 +0330

    #56- uploading file using UploadFile is successfull

--- myupload.py
+++ myupload.py
@@ -1,6 +1,15 @@
-from fastapi import APIRouter, File
+from fastapi import APIRouter, File, UploadFile
+import shutil
 
 upload_router = APIRouter()
+
 @upload_router.post("/file/")
 def upload_file(file: bytes = File()):
     return {"file_size": len(file)}
+
+@upload_router.post("/uploadfile1/")
+def upload_file1(file: UploadFile):
+    return {
+        "filename": file.filename, 
+        "content_type": file.content_type,
+        }
