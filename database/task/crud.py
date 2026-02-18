4df398f8d94c80f0522c168e1a91805434d347f2
hoseinAkbari97 <hossein16akbari@gmail.com>
2026-02-18 11:17:37 +0330

    #66- CRUD methods for tasks, database - put-patch-delete operations

--- database/task/crud.py
+++ database/task/crud.py
@@ -12,3 +12,27 @@ def getAll(db: Session):
     tasks = db.query(models.Task).all()
     return tasks
 
+def create(task: Task, db: Session):
+    taskdb = models.Task(name=task.name, description=task.description, status=task.status)
+    db.add(taskdb)
+    db.commit()
+    db.refresh(taskdb)
+    return taskdb
+
+def update(id: int, task: Task, db: Session):
+    
+    taskdb = getById(id, db)
+    
+    taskdb.name = task.name
+    taskdb.description = task.description
+    taskdb.status = task.status
+    
+    db.add(taskdb)
+    db.commit()
+    db.refresh(taskdb)
+    return taskdb
+
+def delete(task: Task, db: Session):
+    db.delete(task)
+    db.commit()
+    
